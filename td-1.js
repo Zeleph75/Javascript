ce4d63bb7ee8eb63816268f6fb5f60e7767ecf97
Zeleph75 <hugoviolaps4pro75@gmail.com>
2022-05-10 08:52:16 +0200

    modif HTML

--- td-1.js
+++ td-1.js
@@ -4,16 +4,22 @@ const pList = document.querySelector('#myList p');
 const pArray = document.querySelector('#myArray p');
 const myArray = ['une ligne','autre chose','et encore'];
 
-function addText() {
+function addText()
+{
     if (txtToAdd.value !== '')
     {
-        pList.innerText += txtToAdd.value+ '\n'
+        pList.innerText += txtToAdd.value + '\n';
         txtToAdd.value = '';
         
     }
     else {
-        alert('Veuillez entrez un texte')
+        alert('Veuillez entrez un texte');
     }
     txtToAdd.focus();
 }
-
+function updateFromArray()
+{
+    pArray.innerText += myArray[i] + 'n';
+    
+    
+}
